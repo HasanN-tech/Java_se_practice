8461041ed152b54de88a66dd9d3aa09e9d52fd8c
Mohd Hasan <125153993+HasanN-tech@users.noreply.github.com>
2023-08-28 23:06:41 +0930

    Update Main.java

--- com/hasan/Main.java
+++ com/hasan/Main.java
@@ -2,6 +2,44 @@
 package com.hasan;
 public class Main {
   public static void main(String args[]){
-    
+//    ArrayList l= new ArrayList();
+//    l.add(72);
+//    l.add(12);
+//    l.add(22);
+//    l.add(32);
+//    l.add("A");
+//    l.add(2,"tewnty two");
+//    System.out.println(l);
+//    l.remove(2);
+//    l.remove(3);
+//    System.out.println(l);
+//    l.add(0,10);
+//    l.add(0,15);
+//
+//    System.out.println(l.contains(72));
+//    System.out.println(l);
+
+    ArrayList l2=new ArrayList(6);
+    l2.add(1);
+    l2.add(2);
+    l2.add(3);
+    l2.add(4);
+    l2.add(5);
+    l2.add(6);
+    l2.add(7);
+    System.out.println(l2);
+    l2.set(0,0);
+    System.out.println(l2.size());
+    System.out.println(l2);
+    System.out.println("**************************************************");
+    ArrayList l3=new ArrayList(l2);
+    l3.add(10);
+    l3.add(20);
+    l3.add(30);
+    l3.add(40);
+    l3.add(50);
+    System.out.println(l3);
+  }
+
   }
 }
